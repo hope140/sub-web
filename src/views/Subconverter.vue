e38dbf989431d9d5b38a0de4b135c1276792e83e
Hope <lucifinil006@foxmail.com>
2021-03-22 18:34:18 +0800

    增加一位热心网友的后端

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -257,12 +257,13 @@ export default {
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "api.hope140.live (如有问题请反馈至tg-@hope140）": "https://api.hope140.live/sub?",
-          "api.tsutsu.cc (つつ提供-稳定）": "http://api.tsutsu.cc:520/sub?",
           "api-cf.tsutsu.cc (つつ提供-稳定）": "https://api-cf.tsutsu.cc/sub?",
+          "sub.proxypoolv2.tk (Allen Xu提供-稳定)": "sub.proxypoolv2.tk",
+          "api.tsutsu.cc (つつ提供-不是很稳定）": "http://api.tsutsu.cc:520/sub?",
           "subcon.dlj.tf (subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
-          "api.dler.io (sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
-          "api.wcc.best (sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
+          "api.dler.io (sub作者&lhie1提供)": "https://api.dler.io/sub?",
+          "api.wcc.best (sub-web作者提供)": "https://api.wcc.best/sub?",
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
